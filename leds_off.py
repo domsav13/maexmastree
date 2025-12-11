ab2e3a8cbecbb65a33eaf81e56a5763c1fbcf288
Dominic Savarino <136136566+domsav13@users.noreply.github.com>
2025-12-11 18:43:17 -0500

    Update leds_off.py

--- leds_off.py
+++ leds_off.py
@@ -17,7 +17,7 @@ LED_COUNT = len(coords)
 # -----------------------------
 # Strip configuration
 # -----------------------------
-LED_PIN        = 12
+LED_PIN        = 18
 LED_FREQ_HZ    = 800000
 LED_DMA        = 10
 LED_INVERT     = False
